0f0f816e781dd9b85a1c82fd58adc923ed63db8e
Ryan Matthew Burns <ryan.m.burns123@gmail.com>
2022-09-28 20:18:55 +0000

    It's alive!

--- client.js
+++ client.js
@@ -9,16 +9,22 @@ const connect = function() {
   });
 
   conn.setEncoding('utf-8');
-  
+
   conn.on("connect", () => {
     console.log(`Hello, this is Stacy the computer.\nGood morning, Simon, I trust you slept well.\nWill you be searching for true love today?`);
     conn.write('Name: RMB');
   });
-  
+
+  conn.on("connect", () => {
+    setInterval(() => {
+      conn.write('Move: up');
+    }, 50);
+  });
+
   conn.on('data', (data) => {
     console.log(`incoming data: ${data}`);
   });
-  
+
   return conn;
 };
 
