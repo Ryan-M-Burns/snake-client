02992c8c7fa1285411357ca5526bda6801a85dc8
Ryan Matthew Burns <ryan.m.burns123@gmail.com>
2022-09-28 19:59:44 +0000

    created the client file that we call in play

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require('net');
+
+const connect = function() {
+
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+
+  });
+
+  conn.setEncoding('utf-8');
+  
+  conn.on("connect", (data) => {
+    console.log(`Hello, this is Stacy the computer.\nGood morning, Simon, I trust you slept well.\nWill you be searching for true love today?`);
+  
+  });
+  
+  conn.on('data', (data) => {
+    console.log(`incoming data: ${data}`);
+  });
+  
+  return conn;
+};
+
+module.exports = { connect };
