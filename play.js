725c33796fcf9c636d0195f46419f6f261b274d7
Ryan Matthew Burns <ryan.m.burns123@gmail.com>
2022-09-28 19:53:17 +0000

    fixed play.js

--- play.js
+++ play.js
@@ -9,14 +9,19 @@ const connect = function() {
   });
 
   conn.setEncoding('utf-8');
-
+  
+  conn.on("connect", (data) => {
+    console.log(`Hello, this is Stacy the computer.\nGood morning, Simon, I trust you slept well.\nWill you be searching for true love today?`);
+  
+  });
+  
+  conn.on('data', (data) => {
+    console.log(`incoming data: ${data}`);
+  });
+  
   return conn;
 };
 
+
 console.log("Connecting...");
 connect();
-
-conn.on("connect", () => {
-
-
-});
